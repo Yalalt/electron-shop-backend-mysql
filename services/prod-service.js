83fa4f3568ac16875c9dd0b5a68ced7957192ebe
Yalalt <yalaltgandush@gmail.com>
2023-03-02 12:47:17 +0800

    Some error fix

--- services/prod-service.js
+++ services/prod-service.js
@@ -1,7 +1,7 @@
 import { pool } from "../config/mysql-config";
 
 export async function getEmployees(limit) {
-  const [rows] = await pool.query(`SELECT emp_no FROM salaries linit ${limit}`);
+  const [rows] = await pool.query(`SELECT emp_no FROM salaries linit ${limit || 10}`);
   console.log(rows);
   return rows;
 }
